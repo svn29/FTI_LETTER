bdf9e63d3354187d43002e593876355665b97748
space-stronaut <abelr6099@gmail.com>
2021-12-06 18:30:48 +0700

    add overview

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -154,14 +154,12 @@ Coded by www.creative-tim.com
             <ul class="navbar-nav">
               <li class="nav-item btn-rotate dropdown">
                 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="nc-icon nc-bell-55"></i>
+                  <span>Hi, {{ Auth::user()->nama }}</span>
                   <p>
                     <span class="d-lg-none d-md-block">Some Actions</span>
                   </p>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">Action</a>
-                  <a class="dropdown-item" href="#">Another action</a>
                   <a class="dropdown-item" href="{{ route('logout') }}">Logout</a>
                 </div>
               </li>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,152 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            Halo
+<div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-6">
+      <div class="card card-stats">
+        <div class="card-body ">
+          <div class="row">
+            <div class="col-5 col-md-4">
+              <div class="icon-big text-center icon-warning">
+                <i class="nc-icon nc-globe text-warning"></i>
+              </div>
+            </div>
+            <div class="col-7 col-md-8">
+              <div class="numbers">
+                <p class="card-category">Tugas Pribadi</p>
+                <p class="card-title">{{ count(App\Models\Surat::where('jenis_surat', 'tugas pribadi')->get()) }}<p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="card-body">
-            Hlo
+        <div class="card-footer ">
+          <hr>
+          <div class="stats">
+            <i class="fa fa-refresh"></i>
+            <a href="{{ route('tugas_pribadi.create') }}">Create</a>
+          </div>
         </div>
+      </div>
     </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+      <div class="card card-stats">
+        <div class="card-body ">
+          <div class="row">
+            <div class="col-5 col-md-4">
+              <div class="icon-big text-center icon-warning">
+                <i class="nc-icon nc-money-coins text-success"></i>
+              </div>
+            </div>
+            <div class="col-7 col-md-8">
+              <div class="numbers">
+                <p class="card-category">Tugas Kelompok</p>
+                <p class="card-title">{{ count(App\Models\Surat::where('jenis_surat', 'tugas kelompok')->get()) }}<p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer ">
+          <hr>
+          <div class="stats">
+            <i class="fa fa-calendar-o"></i>
+            <a href="{{ route('tugas_kelompok.create') }}">Create</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+      <div class="card card-stats">
+        <div class="card-body ">
+          <div class="row">
+            <div class="col-5 col-md-4">
+              <div class="icon-big text-center icon-warning">
+                <i class="nc-icon nc-vector text-danger"></i>
+              </div>
+            </div>
+            <div class="col-7 col-md-8">
+              <div class="numbers">
+                <p class="card-category">Tugas Dosen</p>
+                <p class="card-title">{{ count(App\Models\Surat::where('jenis_surat', 'tugas dosen')->get()) }}<p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer ">
+          <hr>
+          <div class="stats">
+            <i class="fa fa-clock-o"></i>
+            <a href="{{ route('tugas_dosen.create') }}">Create</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+      <div class="card card-stats">
+        <div class="card-body ">
+          <div class="row">
+            <div class="col-5 col-md-4">
+              <div class="icon-big text-center icon-warning">
+                <i class="nc-icon nc-favourite-28 text-primary"></i>
+              </div>
+            </div>
+            <div class="col-7 col-md-8">
+              <div class="numbers">
+                <p class="card-category">Berita Acara</p>
+                <p class="card-title">{{ count(App\Models\Surat::where('jenis_surat', 'berita acara')->get()) }}<p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer ">
+          <hr>
+          <div class="stats">
+            <i class="fa fa-refresh"></i>
+            <a href="{{ route('berita_acara.create') }}">Create</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card ">
+        <div class="card-header ">
+          <h5 class="card-title">User</h5>
+          {{-- <p class="card-category">24 Hours performance</p> --}}
+        </div>
+        <div class="card-body ">
+          <table class="table">
+              <thead class="thead-dark">
+                  <tr>
+                    {{-- <th>#</th> --}}
+                    <th scope="col">Jumlah Dosen</th>
+                    <th scope="col">Jumlah Mahasiswa</th>
+                    <th scope="col">Jumlah Admin</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                    <td>
+                        {{ count(App\Models\User::where('role', 'dosen')->get()) }}
+                    </td>
+                    <td>
+                        {{ count(App\Models\User::where('role', 'mahasiswa')->get()) }}
+                    </td>
+                    <td>
+                        {{ count(App\Models\User::where('role', 'admin')->get()) }}
+                    </td>
+                </tr>
+              </tbody>
+          </table>
+        </div>
+        <div class="card-footer ">
+          <hr>
+          <div class="stats">
+            {{-- <i class="fa fa-history"></i> Updated 3 minutes ago --}}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
